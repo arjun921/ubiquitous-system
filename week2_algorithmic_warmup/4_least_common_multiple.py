33ff58b70c11f9c16a6e8d863bb6f808f3c014fa
arjun921 <arjun.sunil@niiconsulting.com>
2019-02-16 13:34:16 +0530

    lcm working! FInally!!

--- week2_algorithmic_warmup/4_least_common_multiple.py
+++ week2_algorithmic_warmup/4_least_common_multiple.py
@@ -1,13 +1,27 @@
 # python3
 
-from math import gcd
+import decimal
+
+def gcd(a,b):
+    while True:
+        if b == 0:
+            return a
+        else:
+            a_pr = a%b
+            a = b
+            b = a_pr
 
 def lcm(a,b):
-    lcm_ = (a*b)/gcd(a,b)
-    print(lcm_)
+    gcd_ = gcd(a,b)
+    # python rounds off decimals beyond certain number hence outputs were varying from expected
+    # using decimal to ensure accuracy.
+    a,b = decimal.Decimal(a),decimal.Decimal(b)
+    lcm_ = (a*b)/gcd_
     return int(lcm_)
 
 a,b = input().split()
 a,b = int(a),int(b)
+# a,b = 226553150,1023473145
 lcm_ = lcm(a,b)
+
 print(lcm_)
--- week2_algorithmic_warmup/5_fibonacci_number_again.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again.py
@@ -1,1 +1,19 @@
-# python3
+#python3
+
+from math import sqrt,log
+
+def fib(n):
+    v1, v2, v3 = 1, 1, 0    # initialise a matrix [[1,1],[1,0]]
+    for rec in bin(n)[3:]:  # perform fast exponentiation of the matrix (quickly raise it to the nth power)
+        calc = v2*v2
+        v1, v2, v3 = v1*v1+calc, (v1+v3)*v2, calc+v3*v3
+        if rec=='1':    v1, v2, v3 = v1+v2, v1, v2
+    return v2
+
+a = input().split()
+n,m = int(a[0]),int(a[1])
+f = fib(n)
+
+print(f)
+print(39679027332006820581608740953902289877834488152161)
+print(f%m)
